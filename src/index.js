781fd49fda6b25e4a5ca83f73a7de5088e678004
MercyMilkahNjokiKahungu <mercy.njoki@student.moringaschool.com>
2022-08-24 11:57:27 +0300

    done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    myTodo(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function myTodo(todo){
+  let li = document.createElement("li")
+  let btn =document.createElement("button")
+  btn.addEventListener("click",handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
